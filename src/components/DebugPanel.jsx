1164711f25c3266d5f8b9cd186ddb3f57f5865d2
sekyeong <sekoonglee@gmail.com>
2025-03-12 01:18:49 +0900

    feat: 폴더구조 세팅 및 패키지 설치 (#1)

---
+++ src/components/DebugPanel.jsx
@@ -0,0 +1,5 @@
+function DebugPanel() {
+  return <div>Debug Panel</div>;
+}
+
+export default DebugPanel;
---
+++ src/pages/Debug.jsx
@@ -0,0 +1,5 @@
+function Debug() {
+  return <div>Debug Page</div>;
+}
+
+export default Debug;
