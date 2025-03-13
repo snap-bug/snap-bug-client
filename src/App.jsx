7b5def94a4d3cd4afe7a33ebfef2fab90b2fa6ed
sekyeong <sekoonglee@gmail.com>
2025-03-13 23:39:55 +0900

    design: 사이드바 메인 영역 구조 설정 (#26)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-function App() {
+import Debug from "./pages/Debug";
+import DebugPanel from "./components/DebugPanel";
+import Footer from "./shared/components/Footer";
+import "./styles/globals.css";
 
+function App() {
   return (
-    <div>Init</div>
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-grow h-3/4">
+        <div className="w-4/12">
+          <DebugPanel />
+        </div>
+        <div className="w-8/12">
+          <Debug />
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
