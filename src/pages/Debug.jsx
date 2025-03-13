bce423f64c451bd0d220f80221b360512b86d096
sekyeong <sekoonglee@gmail.com>
2025-03-13 23:44:35 +0900

    style: tailwind 스타일 조정

--- src/pages/Debug.jsx
+++ src/pages/Debug.jsx
@@ -1,6 +1,6 @@
 function Debug() {
   return (
-    <div className="flex h-dvh w-dvw flex-col items-center justify-center bg-white dark:bg-black">
+    <div className="flex h-dvh items-center justify-center bg-white dark:bg-black">
       Debug Page
     </div>
   );
