2f594a671bb768b9efdf7dced4dfff3dd1f76f71
sekyeong <sekoonglee@gmail.com>
2025-03-13 23:21:22 +0900

    refactor: Footer 시맨틱 태그 적용 및 CSS 변경

--- src/shared/components/Footer.jsx
+++ src/shared/components/Footer.jsx
@@ -2,12 +2,8 @@ const TEAM_MAIL = "snapBug@gmail.com";
 
 function Footer() {
   return (
-    <div className="absolute bottom-5 flex flex-col w-full justify-evenly">
-      <a
-        href="https://github.com/snap-bug"
-        target="_blank"
-        className="text-sm"
-      >
+    <footer className="w-full my-2 py-3 text-center text-sm text-white">
+      <a href="https://github.com/snap-bug" target="_blank" className="text-sm">
         <h2 className="m-1 text-center text-sm">
           <p className="text-yellow-500">@snap-bug</p>
         </h2>
@@ -17,7 +13,7 @@ function Footer() {
           Please email to <b className="text-black">{TEAM_MAIL}</b>
         </a>
       </h2>
-    </div>
+    </footer>
   );
 }
 
