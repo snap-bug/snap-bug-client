7968fb2f3e4425190a36dc55879bb26d275c1859
sekoong <sekoonglee@gmail.com>
2025-03-14 09:34:39 +0900

    design: 사이드바 레이아웃 설정 (#27)
    * design: 사이드바 버튼 영역 UI 배치 설정 (#26)
    
    * design: 사이드바 메인 영역 구조 설정 (#26)

--- src/shared/components/Button.jsx
+++ src/shared/components/Button.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 
-function Button({ onClick, children }) {
+function Button({ onClick, children, disabled }) {
   return (
     <button
       className="flex w-fit flex-row items-center rounded-lg border border-slate-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 transition duration-300 ease-in-out hover:bg-slate-800 hover:text-yellow-300 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-400 dark:bg-gray-700 dark:text-gray-100 dark:hover:border-black dark:hover:bg-yellow-400 dark:hover:text-black dark:focus:ring-sky-700"
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
@@ -14,6 +15,11 @@ function Button({ onClick, children }) {
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  disabled: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  disabled: false,
 };
 
 export default Button;
