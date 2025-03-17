ef2fe1b654ffafad39bf666a6e5871628a9c76c2
dowonee <jjddwwyt@gmail.com>
2025-03-18 01:04:39 +0900

    feat: 엔트리 파일 자동 탐색 기능 추가

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const findEntryFile = (directory) => {
+  const possibleFiles = ["Main.jsx", "main.jsx", "index.jsx", "App.jsx"];
+
+  for (const file of possibleFiles) {
+    const filePath = resolve(__dirname, directory, file);
+
+    if (fs.existsSync(filePath)) {
+      return filePath;
+    }
+  }
+
+  throw new Error("src 폴더에 Main.jsx 또는 main.jsx가 있는지 확인해주세요.");
+};
 
 export default defineConfig({
   plugins: [react()],
