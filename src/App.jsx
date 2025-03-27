d848c7e80bd9b4b432bc8bc6bb37c1ddcc3f49de
정도원 <83629099+dowonee@users.noreply.github.com>
2025-03-27 09:33:30 +0900

    feat: JSON 데이터 기반 UI 구현 및 개선 (#33) (#41)
    * feat: 디버깅 영역용 style 및 preview 컨테이너 DOM 구조 추가
    
    * feat: 상태 클릭시 해당 상태로 돌아가는 기능 구현
    
    * style: 전체적인 UI 비율 개선
    
    * feat: 현재 스냅샷 기준으로 렌더링 및 스크롤 이동 처리 기능 추가
    
    * feat: 클릭 시 해당 상태로 돌아가는 기능 추가
    
    * refactor: 데이터에 맞게 변수 추가, 제거, 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Debug from "./pages/Debug";
 import DebugPanel from "./components/DebugPanel";
 import Footer from "./shared/components/Footer";
 import "./styles/globals.css";
@@ -6,12 +5,16 @@ import "./styles/globals.css";
 function App() {
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex flex-grow h-3/4">
-        <div className="w-4/12">
+      <div className="flex flex-1 overflow-hidden">
+        <div className="w-[30%] h-full overflow-hidden border-r border-gray-300">
           <DebugPanel />
         </div>
-        <div className="w-8/12">
-          <Debug />
+        <div className="w-[70%] bg-gray-100 p-6 h-full overflow-auto">
+          <div id="style-container"></div>
+          <div
+            id="preview-container"
+            className="bg-white p-4 mt-4 rounded shadow-inner h-full overflow-auto"
+          ></div>
         </div>
       </div>
       <Footer />
